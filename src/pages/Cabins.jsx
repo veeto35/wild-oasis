cf5d0b2949e9510be923a8d50be0f67f5831a72e
veeto <killersauros01@gmail.com>
2024-12-29 01:50:08 +0000

    first commit

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+
+function Cabins() {
+  const [showForm,setShowForm] = useState(false);
+
+  return (
+
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter and Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm(show => !show)}>Add new Cabin</Button>
+        { showForm && <CreateCabinForm />}
+      </Row>
+    </>
+  );
+}
+
+export default Cabins;
